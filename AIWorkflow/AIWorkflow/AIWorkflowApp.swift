59f2eeee5f8c6f00d7e7a8c08df781a65ce28b27
Okan Orkun <orkunokann@gmail.com>
2025-11-10 21:14:44 +0300

    Initial Commit

---
+++ AIWorkflow/AIWorkflow/AIWorkflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIWorkflowApp.swift
+//  AIWorkflow
+//
+//  Created by Okan Orkun on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIWorkflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
